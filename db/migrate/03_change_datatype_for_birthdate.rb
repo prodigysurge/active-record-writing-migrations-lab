36a68b4f9dd4cbee63930a414589de6952dde358
prodigysurge <boatinglife6@yahoo.com>
2021-02-15 13:38:39 -0500

    commit tree 2680d16d205cc9680f5d5659b4b51a842f2f97e0

---
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -0,0 +1,12 @@
+# class ChangeDataTypeForBirthdate < ActiveRecord::Migration[5.1]
+#     def change
+#         change_column :students, :birthdate, :datetime
+#     end
+# end
+
+
+class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
+    def change
+        change_column :students, :birthdate, :datetime
+    end
+end
